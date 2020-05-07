9afa56f0d8df53ddccf03ddcf3e3eaacd75b3d89
Ryan Hopson <ryanhopson99@gmail.com>
2020-05-07 20:04:08 +1000

    Completed problem: delete middle of a linked list

--- LinkedList/README.md
+++ LinkedList/README.md
@@ -1,1 +1,4 @@
-LinkedList related problems
+#Linked List Problems#
+My solutions to the following linked list problems
+* Find the middle node of a given linked list
+* Delete the middle node of a given linked list
--- LinkedList/RemoveMiddle.py
+++ LinkedList/RemoveMiddle.py
@@ -6,33 +6,28 @@ Problem - Find the middle node of a linked list
 """
 
 """
-This method requires first finding the size of the linked list 'n' and then
-it moves through the first half 'n/2' part of the list
-"""
-def slow(list):
-    size = list.get_size() #O(n) complexity
-    middle = int(size/2)
-    curr = list.get_head_node()
-    for i in range(0, middle):
-        curr = curr.get_next()
-    return curr.get_data()
-
-"""
 This method utilises two pointers, the first traverses the list 1 node at a time
 and the other 2 nodes at a time. Since n is 2*(n/2) and the second pointer moves
 twice as fast as the first, once the second pointer reaches n, the first will be
-at n/2 which is the middle of the list.  
+at n/2 which is the middle of the list. Once it has found the middle, it deletes
+it from the list
 """
-def fast(list):
+def remove_middle(list):
     fstPtr = list.get_head_node()
+    slwPrev = None
     slwPtr = list.get_head_node()
     while fstPtr is not None:
         fstPtr = fstPtr.get_next()
         if fstPtr is None:
             break #We reached the end, as fstPtr is moving through twice as fast, the slow pointer should be in the middle of the list
         fstPtr = fstPtr.get_next()
+        slwPrev = slwPtr
         slwPtr = slwPtr.get_next()
-    return slwPtr.get_data()
+
+    #Middle has been found, so make slwPrev point to slwPtr's next node
+    print("Removing", slwPtr.get_data(), "from the list!")
+    slwPrev.set_next(slwPtr.get_next())
+
 
 
 #10->25->4->43->30
@@ -56,9 +51,10 @@ print(l1, "| size:", l1.get_size())
 print(l2, "| size:", l2.get_size())
 print(l3, "| size:", l3.get_size())
 
-print("Middle of l1 (slow): ", slow(l1))
-print("Middle of l2 (slow): ", slow(l2))
-print("Middle of l2 (slow): ", slow(l3))
-print("Middle of l1 (fast): ", fast(l1))
-print("Middle of l2 (fast): ", fast(l2))
-print("Middle of l2 (fast): ", fast(l3))
+remove_middle(l1)
+remove_middle(l2)
+remove_middle(l3)
+
+print(l1, "| size:", l1.get_size())
+print(l2, "| size:", l2.get_size())
+print(l3, "| size:", l3.get_size())
